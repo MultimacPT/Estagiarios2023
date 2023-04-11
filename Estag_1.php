b87e8899f3a95ed1fc6fba827dab8008790ed102
figartur <97570942+figartur@users.noreply.github.com>
2023-04-11 17:24:51 +0100

    exec php

---
+++ Estag_1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercicio 01</title>
+    <link rel="stylesheet" href="themes/exec1.min.css" />
+    <link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
+    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+</head>
+<body>
+  <div data-role="page" data-theme="a">
+	<div data-role="header" data-position="inline">
+   <header>
+     <h1>Header!!!</h1>
+   </header>
+  </div>
+
+    <br>
+    <h1>Estagio 2023 =D</h1>
+    <br>
+
+
+   <footer>
+     <p>Author: Artur Figueiredo</p>
+   </footer>
+
+</body>
+</html>
