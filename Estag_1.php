e966eff670f36671f227b864f8fa6e54be0c8ee6
Ricard0Fernandes <ricmanuel2005@gmail.com>
2023-04-12 16:23:04 +0100

    atualizar tema

--- Estag_1.php
+++ Estag_1.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
 <html lang="pt-pt">
 <head>
-	<meta charset="utf-8">
+<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="themes/Escuro.min.css" />
+	<title>jQuery Mobile</title>
+	<link rel="stylesheet" href="themes/tema.min.css" />
 	<link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
 	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
 	<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 </head>
 <body>
-	<div data-role="page" data-theme="a">
+	<div data-role="page" data-theme="b">
 		<div data-role="header" data-position="inline">
-			<h1>Site de Teste</h1>
+			<h1>Formulário Básico</h1>
 		</div>
-		<div data-role="content" data-theme="a">
-			<h1>A usar o Jquery mobile</h1>
-			<p>Neste site foi usado o Jquery mobile.</p>
+		<div data-role="content" data-theme="b">
+			<h1>Formulário</h1>
+			<p>Site de formulário</p>
 		</div>
 
 		<footer>
