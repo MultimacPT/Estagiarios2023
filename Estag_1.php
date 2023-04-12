fc89a5873e75fc21dcccd1be006ce078f0642bc1
Ricard0Fernandes <ricmanuel2005@gmail.com>
2023-04-12 15:16:31 +0100

    jqueryMobile

---
+++ Estag_1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="themes/Escuro.min.css" />
+	<link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
+	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+</head>
+<body>
+	<div data-role="page" data-theme="a">
+		<div data-role="header" data-position="inline">
+			<h1>Site de Teste</h1>
+		</div>
+		<div data-role="content" data-theme="a">
+			<h1>A usar o Jquery mobile</h1>
+			<p>Neste site foi usado o Jquery mobile.</p>
+		</div>
+
+		<footer>
+     		<br>Ricardo Fernandes
+   		</footer>
+	</div>
+</body>
+</html>
