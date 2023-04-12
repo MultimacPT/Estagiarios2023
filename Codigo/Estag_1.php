670def28f5c9dd6917626830a5931f47e97eb90f
Raposeiroandre <andre.alves.raposeiro@gmail.com>
2023-04-12 15:05:03 +0100

    Test Page

---
+++ Codigo/Estag_1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<head>
+    <html lang="pt-pt">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Página</title>
+	<link rel="stylesheet" href="themes/PrimeiroTema.min.css" />
+	<link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
+	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+</head>
+<body>
+	<div data-role="page" data-theme="a">
+		<div data-role="header" data-position="inline">
+			<h1>Aqui está a página</h1>
+		</div>
+		<div data-role="content" data-theme="a">
+			<h1>Aqui está um belo Título</h1>
+			<pre>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                Facilis commodi quidem assumenda mollitia eius ut, 
+                tenetur atque delectus deleniti vero sit quo repudiandae velit odio vitae magnam dolore eum labore?
+			</pre>
+		</div>
+        <footer>
+            <p>André Raposeiro</p>
+        </footer>
+	</div>
+</body>
+</html>
