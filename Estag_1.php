fb492aba6e6bc173b6ae65eef53753b152eaeb90
unknown <filipe.ferreira@dualinfor.pt>
2023-04-12 11:44:43 +0100

    Primeiro Commit

---
+++ Estag_1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Estag_1.css">
+    <title>Estag_1</title>
+</head>
+<header>Header</header>
+<body>
+    <div class = "Titulo" >Primeiro Trabalho de estagio</div>
+    <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero impedit commodi asperiores porro iste in molestiae iusto assumenda earum hic harum, recusandae odio. Magnam, quam assumenda doloribus nihil nisi ducimus.</div>
+    <div class = "Espaco">  </div>
+</body>
+<footer>Filipe Ferreira</footer>
+</html>
