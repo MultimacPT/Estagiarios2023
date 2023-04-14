ddf52f01ed0874fa4b14757b8fae1baea4995375
Ricard0Fernandes <ricmanuel2005@gmail.com>
2023-04-14 16:59:36 +0100

    retirar codigo

--- Estag4_e_5_Test.php
+++ Estag4_e_5_Test.php
@@ -53,9 +53,6 @@
                 <input type="submit" value="Submeter">
             </form>
             <?php
-                ini_set('display_errors', 1);
-                ini_set('display_startup_errors', 1);
-                error_reporting(E_ALL);
 
                 if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     $dataInicio = $_POST["inicio"];
