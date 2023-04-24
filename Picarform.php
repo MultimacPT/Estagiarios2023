36c95267f784f88bd5e1e013160e2faa98f87bfa
Raposeiroandre <andre.alves.raposeiro@gmail.com>
2023-04-24 16:55:51 +0100

    Continuar Projeto

--- Picarform.php
+++ Picarform.php
@@ -1,35 +1,30 @@
 <!DOCTYPE html>
-<html lang="pt-pt">
-<head>
-<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Entradas e saidas</title>
-	<link rel="stylesheet" href="themes/tema.min.css" />
-	<link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
-	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
-	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-	<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-</head>
-<body>
-	<div data-role="page" data-theme="a">
-		<div data-role="header">
-			<h1>Formulário de entradas e saidas</h1>
+	<html lang="pt-pt">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Entradas e saidas</title>
+		<link rel="stylesheet" href="themes/tema.min.css" />
+		<link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
+		<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+	</head>
+	<body>
+		<div data-role="page" data-theme="a">
+			<div data-role="header">
+				<h1>Formulário de entradas e saidas</h1>
+			</div>
+			<div data-role="content" data-theme="a">
+				<form action="Picar.php" method="post">
+					<input type="submit" value="validar entrada">
+				</form>
+				<?php
+					include('PicarGet.php');
+				?>
+			<div data-role="footer" data-theme="a"> 
+				<h4>Multimac</h4> 
+			</div> 
 		</div>
-		<div data-role="content" data-theme="a">
-
-            <form action="Picar.php" method="post">
-                <input type="submit" value="validar entrada">
-            </form>
-
-			<?php
-
-			include('PicarGet.php');
-			?>
-            
-
-		<div data-role="footer" data-theme="a"> 
-		<h4>Multimac</h4> 
-		</div> 
-	</div>
-</body>
+	</body>
 </html>
