c4e61f7d40087999a07d375778d6eebedaa1419a
Ricard0Fernandes <ricmanuel2005@gmail.com>
2023-04-27 17:47:03 +0100

    mapa

--- Picarform.php
+++ Picarform.php
@@ -38,11 +38,32 @@
 
 	</div>
 	<script>
-    var map = L.map('map').setView([38.7238099,-9.1342295], 13);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-      maxZoom: 19,
-    }).addTo(map);
+// Definir a variável do contêiner do mapa
+var mapContainer = document.getElementById('map');
+
+// Definir as opções do mapa
+var mapOptions = {
+  center: [38.7238099,-9.1342295],
+  zoom: 13,
+  
+  // desabilitar o zoom com o scroll do mouse
+  scrollWheelZoom: false
+};
+
+// Criar o mapa Leaflet com as opções definidas
+var map = L.map('map' mapOptions); //Adicionar o o impedimento de mexer e dar zoom
+
+// Adicionar camada do mapa base
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+  maxZoom: 19
+}).addTo(map);
+
+// Adicionar o código para interromper o zoom com o scroll do mouse
+mapContainer.addEventListener('wheel', function(event) {
+  event.preventDefault();
+}, { passive: false });
+	
   </script>
     <script src="main.js"></script>
 </body>
