4f00957ac0b65f6607ac60fdf9c0c175365b066f
Ricard0Fernandes <ricmanuel2005@gmail.com>
2023-04-28 10:07:19 +0100

    ficheiro mapa

--- mainMapa.js
+++ mainMapa.js
@@ -1,27 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-// Definir a variável do contêiner do mapa
-var mapContainer = document.getElementById('map');
+		// Definir a variável do contêiner do mapa
+		var mapContainer = document.getElementById('map');
 
-// Definir as opções do mapa
-var mapOptions = {
-  center: [38.7238099,-9.1342295],
-  zoom: 13,
-  // desabilitar o zoom com o scroll do mouse
-  scrollWheelZoom: false
-};
+		// Definir as opções do mapa
+		var mapOptions = {
+		center: [38.7238099,-9.1342295],
+		zoom: 13,
+		scrollWheelZoom: false,
+    dragging: false,
+    touchZoom: false,
+    zoomControl: false,
+    doubleClickZoom: false
+		};
 
-// Criar o mapa Leaflet com as opções definidas
-var map = L.map('map', mapOptions);
+		// Criar o mapa Leaflet com as opções definidas
+		var map = L.map('map', mapOptions);
 
-// Adicionar camada do mapa base
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-  maxZoom: 19
-}).addTo(map);
+		// Adicionar camada do mapa base
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+		maxZoom: 19
+		}).addTo(map);
 
-// Adicionar o código para interromper o zoom com o scroll do mouse
-mapContainer.addEventListener('wheel', function(event) {
-  event.preventDefault();
-}, { passive: false });
-
-});
+		// Adicionar o código para interromper o zoom com o scroll do mouse
+		mapContainer.addEventListener('wheel', function(event) {
+		event.preventDefault();
+		}, { passive: false });
